8f5151105a0cf5985fde1fc38635d3d3d7cee88a
Hector Velasques <hector.velasques@condor.com.br>
2021-11-04 10:45:53 -0300

    remove swagger

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const { join } = require('path');
-const swaggerJsdoc = require("swagger-jsdoc");
-const swaggerUi = require('swagger-ui-express');
+// const swaggerJsdoc = require("swagger-jsdoc");
+// const swaggerUi = require('swagger-ui-express');
 
 const specs = swaggerJsdoc(require('./config/optionsSwagger'));
 const app = express();
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/v1', require('./app/routes'));
 app.use(express.static(join(__dirname, 'public'), { etag: false, maxAge: '5000' }));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.get('/api-docs.json', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.send(specs);
