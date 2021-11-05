eec54e0ad1bc857f241a35ac06534efd6a052d2c
Hector Velasques <hector.velasques@condor.com.br>
2021-11-05 12:07:17 -0300

    Add createIndex

--- config/mongo.js
+++ config/mongo.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 
-const db = mongoose.createConnection(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-const dbAdmin = mongoose.createConnection(process.env.DB_URI_ADMIN, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.createConnection(process.env.DB_URI, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const dbAdmin = mongoose.createConnection(process.env.DB_URI_ADMIN, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 module.exports = { dbAdmin, db };
