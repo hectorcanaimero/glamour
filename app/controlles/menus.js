3d41b696f94f12e14db93f1c89bcab1126378d5e
Hector Velasques <hector.velasques@condor.com.br>
2021-11-08 11:11:10 -0300

    Add endpoint menusBySlug e menuByCode

--- app/controlles/menus.js
+++ app/controlles/menus.js
@@ -4,10 +4,30 @@ const menus = require('../models/menus');
 const getMenus = async (req, res) => {
   try {
     const items = await menus.find({});
-    res.status(200).send({ items });
-  } catch (e) {
-    httpError(res, e);
-  }
+    res.status(200).send(items);
+  } 
+  catch (e) { httpError(res, e); }
+};
+  const getMenusBySlug = async (req, res) => {
+    const { slug } = req.params;
+    console.log(slug);
+    try {
+      const items = await menus.find({slug});
+      if (!items)  return res.status(400).send({ message: 'Data is not Found!' });
+      return res.status(200).send(items[0]);
+    }
+    catch (e) { httpError(res, e); }
 };
 
-module.exports = { getMenus };
+const getMenusByCode = async (req, res) => {
+    const { code } = req.params;
+    console.log(code);
+    try {
+      const items = await menus.find({codMercadologico: code});
+      if (!items)  return res.status(400).send({ message: 'Data is not Found!' });
+      return res.status(200).send(items);
+    }
+    catch (e) { httpError(res, e); }
+};
+
+module.exports = { getMenus, getMenusBySlug, getMenusByCode };
--- app/routes/menus.js
+++ app/routes/menus.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getMenus }  = require('../controlles/menus');
+const { getMenus, getMenusBySlug, getMenusByCode }  = require('../controlles/menus');
 
 
 
 router.get('/', getMenus);
+router.get('/:slug', getMenusBySlug);
+router.get('/:code', getMenusByCode);
 
 module.exports = router;
