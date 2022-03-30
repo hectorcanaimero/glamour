b42b404779affeb8ca830ecfde91bbd3bfc9d053
Hector Velasques <hector.velasques@condor.com.br>
2022-03-30 09:58:35 -0300

    Add Loja Favorita

--- app/controlles/atacarejo.js
+++ app/controlles/atacarejo.js
@@ -2,21 +2,32 @@ require('dotenv').config();
 const request = require('request');
 const https = require('https');
 
-
-
 const getMaster = async (req, res) => {
   const { collection } = req.params;
   const options = { method: "GET", url: `${process.env.API}/dados/${collection}`, "rejectUnauthorized": false };
   request(options, (error, response, body) => {
     if (error) return res.status(400).send(error);
-    return res.status(200).json(JSON.parse(body));
+    if (collection === 'listaLojaFavorita') return getLoja(res, body);
+    return res.status(200).send(body);
   });
+};
+
+const getLoja = (res, body) => {
+  const data = [];
+  body = JSON.parse(body);
+  const find = body.filter(row => row.id === 603);
+  for (let i = 0; i < body.length; i++) {
+    const element = body[i];
+    element.nome = element.nome.replace('GARANTE', 'GIGANTE');
+    if(element.id !== 603) data.push(element);
+  }
+  return res.status(200).send(data);
 };
 
 const getSearch = async (req, res) => {
   const { cpf, cnpj } = req.headers;
   const { collection } = req.params;
-  const options= { 
+  const options= {
     "rejectUnauthorized": false, headers: {},
     method: "GET", url: `${process.env.API}/manutencao/${collection}`,
   };
@@ -25,10 +36,9 @@ const getSearch = async (req, res) => {
   request(options, (error, response, body) => {
     if (error) return res.status(400).send(error);
     body = JSON.parse(body);
-    return res.send(body);
+    return res.status(200).send(body);
   });
 };
-
 
 const postPessoa = (req, res) => {
   const { collection } = req.params;
--- app/routes/atacarejo.js
+++ app/routes/atacarejo.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const {getMaster, getSearch, postPessoa } = require('../controlles/atacarejo');
+const controller = require('../controlles/atacarejo');
 
-//TODO: Login !
-// router.post('/', createItem);
-// router.get('/', getItems);
-// router.get('/:cpfcnpj', getItem);
-
-router.get('/dados/:collection', getMaster);
-router.get('/manutencao/:collection', getSearch);
-router.post('/manutencao/:collection', postPessoa);
+router.get('/dados/:collection', controller.getMaster);
+router.get('/manutencao/:collection', controller.getSearch);
+router.post('/manutencao/:collection', controller.postPessoa);
 
 module.exports = router;
