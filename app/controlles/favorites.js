76dd8ff940a27bfc31f3f9fd2d809eec8054b57e
Hector Velasquez <knaimero@gmail.com>
2022-05-31 17:31:29 -0300

    Arrumando quando não tem favoritos

--- app/controlles/favorites.js
+++ app/controlles/favorites.js
@@ -8,13 +8,17 @@ const options = { page: 1, limit: 10, collation: { locale:  'pt'} };
 const getFavorite = async (req, res) => {
   try {
     const { cpf, store } = req.params;
+    console.log(cpf);
+    console.log(store);
     // TODO: REDIS
     const cacheKey = `favorites_cpf_${cpf}_store_${store}`;
     const cachedData = await cache.get(cacheKey);
     if (cachedData) return res.status(200).json(cachedData);
     // TODO: MONGO
     const item = await favorites.findOne({ cpf });
+    if(!item) return res.status(404).send({ message: 'Não tem Favoritos' });
     const products = await stores.find({ codLoja: Number(store), host: { $in: item.products } });
+    if(!products) return res.status(404).send({ message: 'Não tem produtos pra o CPF' });
     const items = { cpf: item.cpf, products, createdAt: item.createdAt };
     await cache.save(cacheKey, items, 60*60*24);
     res.status(200).send(items);
